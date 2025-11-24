eedbab9bec6f9d296f7ffc685595273f26ffbe9a
mateispic <matei-ioan.pricop@student.tuiasi.ro>
2025-11-24 18:26:49 +0200

    added aop decorators to my classes

--- data/code_loader.py
+++ data/code_loader.py
@@ -1,6 +1,7 @@
 # Factory Method
 
 from abc import ABC, abstractmethod
+from aop.aspects import log_call, timeit, debug
 
 class CodeLoader(ABC):
     @abstractmethod
@@ -15,12 +16,16 @@ class CodeLoader(ABC):
             return TextInputCodeLoader()
         else:
             raise ValueError("Invalid source type")
-    
+ 
+@timeit 
+@log_call          
 class FileCodeLoader(CodeLoader):
     def load(self, source):
         with open(source, "r", encoding="utf-8") as f:
             return f.read()
-        
+
+@timeit 
+@log_call
 class TextInputCodeLoader(CodeLoader):
     def load(self, source):
         return source
--- training/trainer.py
+++ training/trainer.py
@@ -2,7 +2,7 @@ from typing import Dict, Any
 from data.dataset import Dataset
 from models.service import ModelService
 from events.observer import ProgressSubject, ProgressObserver
-
+from aop.aspects import log_call, timeit, debug
 
 class Trainer(ProgressSubject):
     def __init__(self):
@@ -23,17 +23,23 @@ class Trainer(ProgressSubject):
         for o in self._observers:
             o.update(self, payload)
 
-    # Builder steps 
+    # Builder steps
+    @timeit 
+    @log_call      
     def prepareData(self, dataset: Dataset):
         self.dataset = dataset
         self.notify({"step": "load_data", "progress": 10})
         return self
 
+    @timeit 
+    @log_call      
     def setModel(self, model_strategy: ModelService):
         self.model_strategy = model_strategy
         self.notify({"step": "set_model", "progress": 20})
         return self
 
+    @timeit 
+    @log_call     
     def initializeModel(self):
         if not self.model_strategy:
             raise ValueError("Model strategy not set")
@@ -41,6 +47,8 @@ class Trainer(ProgressSubject):
         self.notify({"step": "init_model", "progress": 40})
         return self
 
+    @timeit 
+    @log_call      
     def fitModel(self):
         if not self.trained_model or not self.dataset:
             raise ValueError("Missing model or dataset")
@@ -49,11 +57,15 @@ class Trainer(ProgressSubject):
         self.notify({"step": "fit", "progress": 80})
         return self
 
+    @timeit 
+    @log_call      
     def evaluateModel(self):
         # aici mai târziu puneți Accuracy, F1 etc.
         self.results["eval_status"] = "not_implemented"
         self.notify({"step": "eval", "progress": 100})
         return self
 
+    @timeit 
+    @log_call
     def build(self):
         return self.trained_model, self.results
